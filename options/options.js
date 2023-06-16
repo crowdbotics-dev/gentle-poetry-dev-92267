649fce4a03f458ffe398e9b226236bc61c644f59
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 23:17:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-poetry-dev-92267",
+  url: "https://gentle-poetry-dev-92267.botics.co",
+  api: "https://gentle-poetry-dev-92267.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
